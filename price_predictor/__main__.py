0e1d3199e159265843e051888808e7d9dc9f1fab
Ludovico Lemma <89784228+ludovicolemma@users.noreply.github.com>
2022-08-13 15:27:34 +0200

    Adding new Feature's control

--- price_predictor/__main__.py
+++ price_predictor/__main__.py
@@ -6,6 +6,8 @@ def main():
     parser = ArgumentParser(description="", add_help=True, usage="""
     {0} [symbol] #look for the symbol on yahoo finance
     {2:{1}} [--date (str)]
+    {2:{1}} [--target (str)]
+    {2:{1}} [--stamps (int)]
     {2:{1}} [--ratio (float)]
     {2:{1}} [--layers (int)]
     {2:{1}} [--epochs (int)]
@@ -14,10 +16,12 @@ def main():
     tomorrow = parser.add_argument_group("Get tomorrow's opening market price",
                                           "Specify a symbol present on yahoo finance, you can set the training to test ratio, the n° of and ephochs of the LSTM. "
                                           "If not specified the parameters will be set as it follows: "
-                                          "date = '2010-07-01', training_to_test_data_ratio = 0.9, \nn_layers = 4, \nn_epochs = 10")
+                                          "date = '2010-07-01', \ntarget_value = 'Open', \ntime_stamps = 30, \ntraining_to_test_data_ratio = 0.9, \nn_layers = 4, \nn_epochs = 10")
     
     tomorrow.add_argument('symbol', type=str, nargs='*', help='Yahoo finance symbol to download the CSV with the data')
     tomorrow.add_argument('-d', '--date', type=str, help='Specify the date from which to start gathering data', metavar = '')
+    tomorrow.add_argument('-t', '--target', type=str, help="Specify a target between Open, Close, High, Low, 'Adj Close' and Volume", metavar = '')
+    tomorrow.add_argument('-s', '--stamps', type=int, help='Specify how many time stamps to have in each record training sequence', metavar = '')
     tomorrow.add_argument('-r', '--ratio', type=float, help='Specify the ratio to split the training and test data', metavar = '')
     tomorrow.add_argument('-l', '--layers', type=int, help='Specify the number of layers of the neural network (if less than 2 is specified it will be 2 by default)', metavar = '')
     tomorrow.add_argument('-e', '--epochs', type=int, help='Specify the number of epochs of the neural network', metavar = '')
@@ -29,8 +33,10 @@ def main():
     
     else:
         #I just need the prediction, no need of the model in the command line, thus [1]
-        quick_tomorrow(code = args.symbol[0], plot  = False,
+        quick_tomorrow(code = args.symbol[0], plot  = True,
                        start_from_date = args.date,
+                       target_value = args.target,
+                       time_stamps = args.stamps,
                        training_to_test_ratio = args.ratio, 
                        n_layers = args.layers,
                        n_epochs = args.epochs)[1]
